03362bb19b1297f1d2366c61d180500ac2dd309c
Leonardo Costa <lbcosta.dev@gmail.com>
2025-09-26 14:21:31 -0300

    add Instruction.String

--- src/compiler/instruction.go
+++ src/compiler/instruction.go
@@ -1,5 +1,7 @@
 package compiler
 
+import "fmt"
+
 // Possible statements
 const (
 	Increment = iota
@@ -11,4 +13,17 @@ type Instruction struct {
 	Label     string
 	Statement int
 	Args      []string
+}
+
+func (instr Instruction) String() string {
+	switch instr.Statement {
+	case Increment:
+		return fmt.Sprintf("%s <- %s + 1", instr.Args[0], instr.Args[0])
+	case Decrement:
+		return fmt.Sprintf("%s <- %s - 1", instr.Args[0], instr.Args[0])
+	case ConditionalBranch:
+		return fmt.Sprintf("IF %s != 0 GOTO %s", instr.Args[0], instr.Args[1])
+	default:
+		return "UNKNOWN INSTRUCTION"
+	}
 }
